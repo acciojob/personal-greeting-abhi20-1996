49d4c2e318963d0f0ef51eb51167a9225075b1bd
abhi20-1996 <abhiiisonkar@gmail.com>
2023-06-17 14:39:18 +0530

    submitting code-Sat Jun 17 2023 14:39:18 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+let [name, setNmae]=useState("");
+
+function enterdText(event){
+         setNmae(event.target.value);
+}
+
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <label for="name">Enter your name:</label>
+        <input onChange={enterdText} type="text" id="name"/>
+        <p>{name}</p>
     </div>
   )
 }
